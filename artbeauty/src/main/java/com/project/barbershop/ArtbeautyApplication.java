c5df3b315d85c57e1511483db3a2eb657c3e341d
José Gabriel da Silva Lima <josegabrielg831@gmail.com>
2023-09-11 23:22:18 -0300

    create project barber shop art beauty

---
+++ artbeauty/src/main/java/com/project/barbershop/ArtbeautyApplication.java
@@ -0,0 +1,13 @@
+package com.project.barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtbeautyApplication.class, args);
+	}
+
+}
---
+++ artbeauty/src/test/java/com/project/barbershop/ArtbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
